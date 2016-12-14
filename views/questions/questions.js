621c5398d61ad22b723600f027dd4852764bdf94
wangleileiwangleilei <876070495@qq.com>
2016-12-14 19:24:18 +0800

    Add files via upload

--- views/questions/questions.js
+++ views/questions/questions.js
@@ -1,1 +1,21 @@
-
+/**
+ * Created by hxsd on 2016/3/16.
+ */
+angular.module("myapp").controller("questionsController",function($scope,$http){
+    $scope.questions = [];
+    var url="question.json";
+    $scope.loadMore=function(){
+        $http.get(url).success(function(data){
+            //  $scope.restaurants=restaurants.restaurants
+            Array.prototype.push.apply($scope.questions,data)
+        }).finally(function(){
+            $scope.$broadcast("scroll.infiniteScrollComplete");
+        })
+    };
+    $scope.load =function(){
+        $http.get(url).finally(function(){
+            $scope.$broadcast("scroll.refreshComplete")
+        })
+    };
+    $scope.loadMore();
+});
