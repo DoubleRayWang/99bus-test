88550cfb9c17b004627cb889f86259f9cf1ea7ce
wangleileiwangleilei <876070495@qq.com>
2016-12-14 19:19:05 +0800

    Add files via upload

--- views/details/details.js
+++ views/details/details.js
@@ -1,1 +1,17 @@
+/**
+ * Created by Administrator on 2016/3/20.
+ */
+angular.module("myapp").controller("detailsController",function($scope,$http){
+    $scope.FlyLifechinas = [];
+    var url="FlyLifechinas.json";
+    $scope.loadMore=function(){
+        $http.get(url).success(function(data){
+            //  $scope.restaurants=restaurants.restaurants
+            Array.prototype.push.apply($scope.FlyLifechinas,data)
+        }).finally(function(){
+            $scope.$broadcast("scroll.infiniteScrollComplete");
+        })
+    };
+    $scope.loadMore();
 
+});
