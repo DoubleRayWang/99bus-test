9bdc92eccb57dc10b96388fbec193be80db83f2a
wangleileiwangleilei <876070495@qq.com>
2016-12-14 19:16:12 +0800

    Add files via upload

--- views/daibu/daibu.js
+++ views/daibu/daibu.js
@@ -1,1 +1,22 @@
+/**
+ * Created by Administrator on 2016/3/21.
+ */
+angular.module("myapp").controller("daibuController",function($scope,$http){
+    $scope.FlyLifechinas = [];
+    var url="FlyLifechinas.json";
+    $scope.loadMore=function(){
+        $http.get(url).success(function(data){
+            //  $scope.restaurants=restaurants.restaurants
+            Array.prototype.push.apply($scope.FlyLifechinas,data)
+        }).finally(function(){
+            $scope.$broadcast("scroll.infiniteScrollComplete");
+        })
+    };
+    $scope.load =function(){
+        $http.get(url).finally(function(){
+            $scope.$broadcast("scroll.refreshComplete")
+        })
+    };
+    $scope.loadMore();
 
+});
